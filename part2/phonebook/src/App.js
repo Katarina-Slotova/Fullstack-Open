5beb967d5e867d75c4d1b318c8efc40b1aadcc59
Katarina-Slotova <katarina.slotova@gmail.com>
2022-10-21 20:31:53 +0300

    add useEffect to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({value, action}) => {
 	return (
@@ -46,15 +47,18 @@ const Persons = ({persons}) => {
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-	])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [displayFilter, setDisplayFilter] = useState('')
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => {
+				setPersons(response.data)
+			})
+	}, [])
 
 	const displayFilteredContacts = persons.filter((person) => 
 		person.name.toLowerCase().includes(displayFilter.toLowerCase(displayFilter)))
