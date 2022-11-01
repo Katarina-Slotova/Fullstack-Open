d2c7046954d0d6ddac533bbcb89f396939d60bfd
Katarina Slotova <kslotova@c2r2p11.hive.fi>
2022-11-01 09:30:27 +0200

    Add counter practice exercise.

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+/* import { createStore } from 'redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+	// action = a plain JS object that has a type field 
+	// action = an event that describes something that happened in the application
+	// type field should be a string that gives this action a descriptive name, tells about the specific thing that happend
+	// action object can have other fields with additional information about what happened; by convention, we put that information in a field called payload
+	const addTodoAction = {
+		type: 'INCREMENT'
+		// payload: 'Buy milk'
+	}
+
+	// reducer is a function that receives the current state in the store and an action object, 
+	// decides how to update the state if necessary,
+	// and returns the new state: (state, action) => newState
+	// reducer = an event listener which handles events based on the received action (event) type.
+	// reducer is most commpnly written with switch statement
+	// reducer is never supposed to be called directly from the applications code,
+	// reducer is only given as a parameter to the createStore-function which creates the store
+	const counterReducer = (state = 0, action) => {
+		switch (action.type) {
+			case 'INCREMENT':
+				return state + 1
+			case 'DECREMENT':
+				return state - 1
+			case 'ZERO':
+				return 0
+			default:
+				return state
+		}
+	}
+
+	// store uses the reducer to handle actions, 
+	const store = createStore(counterReducer)
+
+	//which are dispatched or 'sent' to the store with its dispatch-method
+	store.dispatch({ type: 'INCREMENT' })
+
+	// find out the state of the store using the method getStat:
+ 	console.log(store.getState())
+	store.dispatch({ type: 'INCREMENT' })
+	store.dispatch({ type: 'INCREMENT' })
+	store.dispatch({ type: 'INCREMENT' })
+	console.log(store.getState())
+	store.dispatch({ type: 'ZERO' })
+	store.dispatch({ type: 'DECREMENT' })
+	console.log(store.getState())
+
+	// subscribe method which is used to create callback functions the store calls whenever an action is dispatched to the store
+	store.subscribe(() => {
+		const storeNow = store.getState()
+		console.log(storeNow)
+	})
+	return (
+		<>
+
+		</>
+	);
 }
 
 export default App;
+ */
--- counter/src/index.js
+++ counter/src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createStore } from 'redux'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button 
+        onClick={e => store.dispatch({ type: 'INCREMENT' })}
+      >
+        plus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'DECREMENT' })}
+      >
+        minus
+      </button>
+      <button 
+        onClick={e => store.dispatch({ type: 'ZERO' })}
+      >
+        zero
+      </button>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => root.render(<App />)
+
+renderApp()
+store.subscribe(renderApp)
